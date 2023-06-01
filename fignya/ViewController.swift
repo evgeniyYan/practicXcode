3ff14267f1fc228d184da9f017b7bd6f0cd07560
Evgeniy Yanushkevich <jonytrees@gmail.com>
2023-06-01 18:49:35 +0600

    стилизовал subView и добавил к view, сделал разметку

--- fignya/ViewController.swift
+++ fignya/ViewController.swift
@@ -9,20 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let subView = UIView()
+    let subView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray4
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "rergerg"
-        view.backgroundColor = .red
+        view.backgroundColor = .systemGray6
         
-        subView.backgroundColor = .gray
+        view.addSubview(subView)
     }
 
 
     override func viewDidLayoutSubviews() {
-        subView.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
+        subView.frame = CGRect(x: 15, y: self.view.frame.height / 3, width: view.frame.width - 30, height: self.view.frame.width / 2)
     }
 }
 
