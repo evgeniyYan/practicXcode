e93641c368fd6a5846fb4d92a1d460093985dfdb
Evgeniy Yanushkevich <jonytrees@gmail.com>
2023-06-01 18:33:01 +0600

    Initial Commit

---
+++ fignya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fignya
+//
+//  Created by Евгений Янушкевич on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
